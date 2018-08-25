19960bae75eac5a79705c975e614ee72a010552b
Deepak <er.d.ahuja@gmail.com>
2018-08-25 17:42:19 +0530

    fix#73 pass 'a' as minor to binding.genSaltSync

--- tests/suite.js
+++ tests/suite.js
@@ -177,9 +177,9 @@ module.exports = {
 
         "roundsOOB": function(test) {
             var salt1 = bcrypt.genSaltSync(0), // $10$ like not set
-                salt2 = binding.genSaltSync(0);
+                salt2 = binding.genSaltSync(0, 'a');
             test.strictEqual(salt1.substring(0, 7), "$2a$10$");
-            test.strictEqual(salt2.substring(0, 7), "$2b$10$");
+            test.strictEqual(salt2.substring(0, 7), "$2a$10$");
 
             salt1 = bcrypt.genSaltSync(3); // $04$ is lower cap
             salt2 = bcrypt.genSaltSync(3);
